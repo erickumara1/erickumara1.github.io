7f5c681eb0269df0edfea1fac9aa5e5e69ce6644
Eric <ekumara@andrew.cmu.edu>
2024-11-18 11:49:12 -0500

    updated website and started password manager project

---
+++ password_manager_project/vault_db.sql
@@ -0,0 +1,14 @@
+--the database will have 3 columns, URL, Username, Password
+-- the values will be hashed with salting using SHA-256
+-- the master key of the PM is the initialization vector for SHA-256
+-- output is 256 bits = 32 bytes = 64 charachter, 1 bytes = 2 charachter
+
+CREATE TABLE vault_table
+(
+    url VARCHAR(64) NOT NULL,
+    username VARCHAR(64) NOT NULL,
+    password VARCHAR(64) NOT NULL
+);
+
+INSERT INTO vault_table
+VALUES('test_url','test_username','test_password');
